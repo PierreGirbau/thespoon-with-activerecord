f9d37832048b03b5e14b0c5627573ac3426660d3
Pierre Girbau <pierregirbau@gmail.com>
2018-02-13 10:40:05 +0100

    Created all routes, views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #CRUD
+  resources :restaurants
+  # #CREATE
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
 
-  #CREATE
-  get 'restaurants/new', to: 'restaurants#new'
-  post 'restaurants', to: 'restaurants#create'
+  # #READ
+  # #Read all
+  # get 'restaurants', to: 'restaurants#index'
+  # #Read one
+  # get 'restaurants/:id', to: 'restaurants#show'
 
-  #READ
-  #Read all
-  get 'restaurants', to: 'restaurants#index'
-  #Read one
-  get 'restaurants/:id', to: 'restaurants#show'
+  # #UPDATE
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants', to: 'restaurants#update'
 
-  #UPDATE
-  get 'restaurants/:id/edit', to: 'restaurants#edit'
-  patch 'restaurants', to: 'restaurants#update'
-
-  #DELETE
-  delete 'restaurants/:id', to: 'restaurants#destroy'
+  # #DELETE
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
 end
